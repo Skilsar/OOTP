93efcc36e03b9b5a6cd9464203a542c6daae8450
Skilsar <skilsar@gmail.com>
2021-10-18 21:37:27 +0300

    Lab_03
    Вторая лабораторная по ООТП

---
+++ LR_03/LR_03/LR_03/Bus.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LR_03
+{
+    partial class Bus
+    {
+
+        private string lastName; //Фамилия
+        private string inizial;  //Инициалы водителя
+        private int idBus;          //Номер автобуса  
+        private int lineId;      //Номер маршрута 
+        const string brand = "Mercedes Benz"; //Марка
+        private int exp;         //Год начала эксплуатации
+        private int mileage;     //Пробег
+        private int age;         //Возрост автобуса
+        private int id;          //Хеш-код
+
+        //Создать массив объектов. Вывести:
+        //a) список автобусов для заданного номера маршрута;
+        //b) список автобусов, которые эксплуатируются больше заданного срока;
+
+
+        public Bus() { }
+
+        public Bus(string lastName, string inizial, int idBus, int lineId, int exp, int mileage, int age) //конструктор
+        {
+
+            this.lastName = lastName;
+            this.inizial = inizial;
+            this.idBus = idBus;
+            this.lineId = lineId;
+            this.exp = exp;
+            this.mileage = mileage;
+            this.id = this.GetHashCode();
+            this.age = age;
+
+            //проверка корректности года эксплуатации
+            if (exp < 1900 || exp > 2021)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Warning! Для автобуса №{idBus} задан неверный год эксплуатации!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            //проверка корректности Фамилии (Наличие цифр)
+            for (int i = 0; i < lastName.Length; i++)
+            {
+                if (Char.IsDigit(lastName, i) == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Error: Фамилия водителя автобуса №{idBus} содержит цифры!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    goto inizialError;
+
+                }
+            }
+
+        //проверка корректности И.О. (Наличие цифр)
+        inizialError:;
+            for (int i = 0; i < inizial.Length; i++)
+            {
+                if (Char.IsDigit(inizial, i) == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Error: Инициалы водителя автобуса №{idBus} содержат цифры!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    goto testErrorTest;
+
+                }
+            }
+        testErrorTest:;
+
+        }
+
+
+
+        public int ID
+        {
+            get => id;// read only
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+
+        }
+        public string Inizials
+        {
+            get
+            {
+                return inizial;
+            }
+            set
+            {
+                inizial = value;
+            }
+
+        }
+        public int BusNumber
+        {
+            get
+            {
+                return idBus;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    idBus = value;
+                }
+                idBus = value;
+            }
+        }
+
+        public int LineNumber
+        {
+            get
+            {
+                return lineId;
+            }
+            private set //для одного из свойств ограните доступ по set
+            {
+                lineId = value;
+            }
+        }
+
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+
+        }
+
+        public int Exploit
+        {
+            get
+            {
+                return exp;
+            }
+            set
+            {
+                exp = value;
+            }
+        }
+
+        public int Mileage
+        {
+            get
+            {
+                return mileage;
+            }
+            set
+            {
+                mileage = value;
+            }
+
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        public void GetBusInfo(Bus bus)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"GetBusInfo (Информация о автобусе №{bus.idBus}): ");
+            Console.WriteLine($"id: {bus.id} \n" +
+                $"lastName: {bus.lastName} \n" +
+                $"inizials: {bus.inizial} \n" +
+                $"brand:{brand} \n" +
+                $"busNumber: {bus.idBus} \n" +
+                $"tourNumber: {bus.lineId} \n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("\nToString (значения строк):");
+            return $"id: {ID} \n" +
+                $"lastName: {LastName} \n" +
+                $"inizials: {Inizials} \n" +
+                $"brand:{brand} \n" +
+                $"busNumber: {BusNumber} \n" +
+                $"tourNumber: {lineId} \n";
+
+        }
+
+        public void GetBusAgeInfo(Bus bus)
+        {
+
+            Console.WriteLine($"Bus number: {bus.idBus}; Age: {bus.age}.")
+             ;
+        }
+        public override bool Equals(object bus)
+        {
+            if (bus.GetType() != this.GetType()) return false;
+
+            Bus person = (Bus)bus;
+            return (this.LastName == person.LastName);
+        }
+    }
+}
---
+++ LR_03/LR_03/LR_03/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LR_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus bus1 = new Bus("Ivanov6", "A.A.", 6, 101, 2017, 5000, 12);
+            Bus bus2 = new Bus("Petrov", "A.B.", 11, 202, 2018, 4000, 48);
+            Bus bus3 = new Bus("Sidorov", "A.C.", 11, 303, 2019, 3000, 34);
+            Bus bus4 = new Bus("Pupkin", "A9.D.", 45, 404, 2100, -1000, 3);
+            Bus bus5 = new Bus("Igorekov", "A.E.", 24, 505, 2007, 120000, 18);
+
+
+            Console.WriteLine(bus1.ToString());
+
+            bus1.GetBusInfo(bus1);
+            bus2.GetBusInfo(bus2);
+            bus3.GetBusInfo(bus3);
+            bus4.GetBusInfo(bus4);
+            bus5.GetBusInfo(bus5);
+
+            Console.WriteLine($"Age of buses:");
+
+            bus1.GetBusAgeInfo(bus1);
+            bus2.GetBusAgeInfo(bus2);
+            bus3.GetBusAgeInfo(bus3);
+            bus4.GetBusAgeInfo(bus4);
+            bus5.GetBusAgeInfo(bus5);
+
+            Arrays();
+
+
+            Console.ReadKey();
+        }
+
+        public static void Arrays()
+        {
+            var Buses = new Bus[2];
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n\nСоздание массива объектов");
+            Console.ForegroundColor = ConsoleColor.White;
+            Buses[0] = new Bus("Vinny", "M.A.", 777, 601, 6, 2040, 12);
+            Buses[1] = new Bus("Suvor", "A.A.", 555, 701, 5, 2070, 15);
+            foreach (var i in Buses)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            foreach (var bus in Buses)
+                if (bus.LineNumber == 601)  //список автобусов для заданного номера маршрута;
+                    Console.WriteLine($"Номер автобуса для заданного номера маршрута: {bus.BusNumber}");
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            foreach (var bus in Buses)
+                if (DateTime.Now.Year - bus.Exploit > 12)   //список автобусов, которые эксплуатируются больше заданного срока;
+            Console.WriteLine($"Номер автобуса, который эксплуатируется больше заданного срока: {bus.BusNumber}");
+
+
+            //анонимный тип 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nАнонимный тип. Задание и вывод параметров.");
+            var busAnonimus = new { Name = "BOBBY", busNumber = 141, tourNumber = 25, exploit = 12, mileage = 2010 };
+            Console.WriteLine($"Name:" + busAnonimus.Name + ";  " + "Bus number:" + busAnonimus.busNumber + ";  " + "Tour number:" + busAnonimus.tourNumber + ";  " + "Exploit:" + busAnonimus.exploit + ";  " + "Mileage:" + busAnonimus.mileage + ";  ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
