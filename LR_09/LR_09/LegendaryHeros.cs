0228fc1982285eb60cb947bc7dc02c831e8a0573
Skilsar <skilsar@gmail.com>
2021-11-15 22:22:31 +0300

    LR_09
    Add LR_09

---
+++ LR_09/LR_09/LegendaryHeros.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace LR_09
+{
+    public class LegendaryHeros
+    {
+        public int HitPoints;
+        public string Name;
+        public string TypeHeros;
+
+        public LegendaryHeros(int HitPoint, string Name)
+        {
+            this.HitPoints = HitPoint;
+            this.Name = Name;
+        }
+
+        public void Attack()
+        {
+            Random DiffHit = new Random();
+            int DiffHitValue = DiffHit.Next(0, 150);
+            this.HitPoints = this.HitPoints - DiffHitValue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"\nАтакован объект {this.Name}!\nНанесено урона: {DiffHitValue}.\nТекущее здоровье объекта: {this.HitPoints}\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public void Health()
+        {
+            Random HealHit = new Random();
+            int HealHitValue = HealHit.Next(50, 100);
+            this.HitPoints = this.HitPoints + HealHitValue;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"\nИсцелен объект {this.Name}!\nДобавлено здоровья: {HealHitValue}.\nТекущее здоровье объекта: {this.HitPoints}\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+
+    public class Hero : LegendaryHeros
+    {
+        public Hero(int HitPoint, string Name):base(HitPoint, Name)
+        {
+            this.TypeHeros = "Герой";
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Создан новый объект:\nИмя: {this.Name}\nТип: {this.TypeHeros}\nУровень здоровья: {this.HitPoints}\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+
+    public class Undead : LegendaryHeros
+    {
+        public Undead(int HitPoint, string Name) : base(HitPoint, Name)
+        {
+            this.TypeHeros = "Нежить";
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Создан новый объект:\nИмя: {this.Name}\nТип: {this.TypeHeros}\nУровень здоровья: {this.HitPoints}\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ LR_09/LR_09/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LR_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero HeroOne = new Hero(1000, "HeroOne");
+            Undead UndeadOne = new Undead(300, "Sceleton");
+            HeroOne.Attack();
+            UndeadOne.Health();
+
+
+            Console.WriteLine("\n------------Работа со строками---------------\n");
+
+            string str = "Работа со строками, и как бы да, но нет, когда всегда...";
+            Console.WriteLine("Исходная строка: " + str);
+            Func<string, string> A = null;
+            A = ChangeString.Remove;
+            Console.WriteLine("Без знаков препинания: {0}", A(str));
+            A = ChangeString.AddToString;
+            Console.WriteLine("Добавление строки: {0}", A(str));
+            A = ChangeString.Upper;
+            Console.WriteLine("Заглавные буквы: {0}", str = A(str));
+            A = ChangeString.Lower;
+            Console.WriteLine("Прописные: {0}", A(str));
+        }
+    }
+}
